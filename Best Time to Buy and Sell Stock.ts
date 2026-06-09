fb89b28f7c30a6ea7c20da63e1a9d65d4e9b15b3
ocean-snail <earthblue0000a0@gmail.com>
2026-06-09 10:50:06 +0100

    Best Time to Buy and Sell Stock revised

--- Best Time to Buy and Sell Stock.ts
+++ Best Time to Buy and Sell Stock.ts
@@ -16,6 +16,8 @@ Output: 5
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 */
+
+// Time - O(n), Space - O(1)
 function maxProfit1(prices: number[]): number {
   let minPrice = Infinity;
   let maxProfit = 0;
@@ -31,29 +33,29 @@ function maxProfit1(prices: number[]): number {
   return maxProfit;
 }
 
-function maxProfit2(prices: number[]): number {
-  if (prices.length === 1) return 0;
+// function maxProfit2(prices: number[]): number {
+//   if (prices.length === 1) return 0;
 
-  let profit = 0;
-  let max = prices[0];
-  let min = prices[0];
-  if (min === undefined) return 0;
+//   let profit = 0;
+//   let max = prices[0];
+//   let min = prices[0];
+//   if (min === undefined) return 0;
 
-  for (let i = 0; i < prices.length; i++) {
-    if (min > prices[i]) {
-      min = prices[i];
-      max = prices[i];
-    } else if (max < prices[i]) {
-      max = prices[i];
-    } else {
-      continue;
-    }
-    if (profit < max - min) {
-      profit = max - min;
-    }
-  }
-  return profit;
-}
+//   for (let i = 0; i < prices.length; i++) {
+//     if (min > prices[i]) {
+//       min = prices[i];
+//       max = prices[i];
+//     } else if (max < prices[i]) {
+//       max = prices[i];
+//     } else {
+//       continue;
+//     }
+//     if (profit < max - min) {
+//       profit = max - min;
+//     }
+//   }
+//   return profit;
+// }
 
 function logResult(prices: number[]) {
   const result = maxProfit1(prices);
