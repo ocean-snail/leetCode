39de8db899957788e01fe64f1d9942d01dce8256
ocean-snail <earthblue0000a0@gmail.com>
2026-06-29 20:22:13 +0100

    fixed sliding window example

---
+++ DataStructures/Array/FixedSlidingWindowExample.java
@@ -0,0 +1,26 @@
+public class FixedSlidingWindowExample {
+
+    public static int maxSumSubArrayOfSizeK(int[] nums, int k) {
+        int windowSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int right = 0; right < nums.length; right++) {
+            windowSum += nums[right];
+
+            if (right >= k - 1) {
+                maxSum = Math.max(maxSum, windowSum);
+
+                int left = right - k + 1;
+                windowSum -= nums[left];
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 1, 5, 1, 3, 2 };
+
+        System.out.println(maxSumSubArrayOfSizeK(nums, 3)); // 9
+    }
+}
