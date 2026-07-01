896d9dbdd6ccb2f2433ba955f505767afc591587
ocean-snail <ocean-snail@users.noreply.github.com>
2026-07-01 11:34:24 +0100

    Two Pointers

---
+++ DataStructures/Array/TwoPointers/TwoPointers.java
@@ -0,0 +1,24 @@
+/*
+? Common Signals
+* Pair search
+* Reverse
+* Sorted array
+* Palindrome
+* Remove duplicates
+* Linked list cycle
+Two pointers exist.
+↓
+Compare something.
+↓
+Use a rule.
+↓
+Move one or both pointers.
+↓
+Never revisit solved positions.
+*/
+public class TwoPointers {
+    public static void main(String[] args) {
+        System.out.println("Two Pointers Class");
+    }
+
+}
